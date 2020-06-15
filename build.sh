b18945c3e521f03c5778a21ebc316e6c78da36f0
Michele Beccalossi <beccalossi.michele@gmail.com>
2020-06-15 11:00:23 +0200

    Rename variables for clarity

--- build.sh
+++ build.sh
@@ -17,202 +17,202 @@
 
 # # # SET DEVICE AND KERNEL NAME # # #
 
-PRODUCT_DEVICE=oneplus5
-PRODUCT_DEVICE_ALIAS=oneplus_msm8998
-PRODUCT_NAME=glowing_potato
+DEVICE_NAME=oneplus5
+DEVICE_SOC=oneplus_msm8998
+PROJECT_NAME=glowing_potato
 
 
 # # # SET TOOLS PARAMETERS # # #
 
 USE_CCACHE=true
-USE_CROSS_COMPILE_REPO=true
+USE_GCC_REMOTE=true
 
-CROSS_COMPILE_NAME=aarch64-linux-android-4.9
-CROSS_COMPILE_SUFFIX=aarch64-linux-android-
-if [ "${USE_CROSS_COMPILE_REPO}" == true ]; then
-  CROSS_COMPILE_REPO=https://source.codeaurora.org/quic/la/platform/prebuilts/gcc/linux-x86/aarch64/${CROSS_COMPILE_NAME}
-  CROSS_COMPILE_BRANCH=keystone/p-keystone-qcom-release
+GCC_NAME=aarch64-linux-android-4.9
+GCC_BINARY=aarch64-linux-android-
+if [ "${USE_GCC_REMOTE}" == true ]; then
+  GCC_REMOTE=https://source.codeaurora.org/quic/la/platform/prebuilts/gcc/linux-x86/aarch64/${GCC_NAME}
+  GCC_BRANCH=keystone/p-keystone-qcom-release
 fi
-ZIP_TEMPLATE_REPO=https://github.com/kylothow/AnyKernel3.git
-ZIP_TEMPLATE_BRANCH=android-10
+TEMPLATE_REMOTE=https://github.com/kylothow/AnyKernel3.git
+TEMPLATE_BRANCH=android-10
 
 
 # # # SCRIPT INIT # # #
 
 tput reset
 
-cd ../${PRODUCT_DEVICE} 2>/dev/null || \
-    cd ../*${PRODUCT_DEVICE_ALIAS} 2>/dev/null || \
-    cd ../${PRODUCT_NAME} 2>/dev/null || exit 1
+cd ../${DEVICE_NAME} 2>/dev/null || \
+    cd ../*${DEVICE_SOC} 2>/dev/null || \
+    cd ../${PROJECT_NAME} 2>/dev/null || exit 1
 
 
 # # # SET LOCAL VARIABLES # # #
 
-BUILD_DIR=$(pwd)
-BUILD_DIR_NAME=$(basename ${BUILD_DIR})
-BUILD_DIR_ROOT=$(dirname ${BUILD_DIR})
+KERNEL_SRC_DIR=$(pwd)
+KERNEL_SRC_DIR_NAME=$(basename ${KERNEL_SRC_DIR})
+KERNEL_SRC_DIR_PARENT=$(dirname ${KERNEL_SRC_DIR})
 
-BUILD_HOST_ARCH=$(uname -m)
-BUILD_JOB_NUMBER=$(nproc --all)
-CROSS_COMPILE_PATH=${BUILD_DIR_ROOT}/gcc/${CROSS_COMPILE_NAME}
+HOST_ARCH=$(uname -m)
+HOST_PROC_NUMBER=$(nproc --all)
+GCC_DIR=${KERNEL_SRC_DIR_PARENT}/gcc/${GCC_NAME}
 
 BUILD_REVISION=$(git rev-parse HEAD | cut -c -7)
 BUILD_TIMESTAMP=$(date '+%Y%m%d')
 
-MAKEFILE=${BUILD_DIR}/Makefile
+MAKEFILE=${KERNEL_SRC_DIR}/Makefile
 MAKEFILE_VERSION=$(grep -Po -m 1 '(?<=VERSION = ).*' ${MAKEFILE})
 MAKEFILE_PATCHLEVEL=$(grep -Po -m 1 '(?<=PATCHLEVEL = ).*' ${MAKEFILE})
 MAKEFILE_SUBLEVEL=$(grep -Po -m 1 '(?<=SUBLEVEL = ).*' ${MAKEFILE})
 LINUX_VERSION=${MAKEFILE_VERSION}.${MAKEFILE_PATCHLEVEL}.${MAKEFILE_SUBLEVEL}
-if [ -f "${BUILD_DIR}/arch/arm64/configs/${PRODUCT_DEVICE}_defconfig" ]; then
-  KERNEL_DEFCONFIG=${PRODUCT_DEVICE}_defconfig
+if [ -f "${KERNEL_SRC_DIR}/arch/arm64/configs/${DEVICE_NAME}_defconfig" ]; then
+  KERNEL_DEFCONFIG=${DEVICE_NAME}_defconfig
 else
   KERNEL_DEFCONFIG=msmcortex-perf_defconfig
 fi
 
-BUILD_DIR_OUT=${BUILD_DIR_ROOT}/${BUILD_DIR_NAME}_out
-BUILD_DIR_OUT_OBJ=${BUILD_DIR_OUT}/KERNEL_OBJ
-BUILD_DIR_ZIP_TEMPLATE=${BUILD_DIR_OUT}/AnyKernel3
-KERNEL_IMG=${BUILD_DIR_ZIP_TEMPLATE}/Image.gz-dtb
-KERNEL_MOD_SYSTEM=${BUILD_DIR_ZIP_TEMPLATE}/modules/system/lib/modules
-KERNEL_MOD_VENDOR=${BUILD_DIR_ZIP_TEMPLATE}/modules/vendor/lib/modules
-PACKAGE_NAME=${PRODUCT_NAME}-${PRODUCT_DEVICE}-${BUILD_TIMESTAMP}-${BUILD_REVISION}.zip
-PACKAGE_PATH=${BUILD_DIR_OUT}/${PACKAGE_NAME}
+OUT_DIR=${KERNEL_SRC_DIR_PARENT}/${KERNEL_SRC_DIR_NAME}_out
+KERNEL_OUT_DIR=${OUT_DIR}/KERNEL_OBJ
+TEMPLATE_SRC_DIR=${OUT_DIR}/AnyKernel3
+KERNEL_IMG=${TEMPLATE_SRC_DIR}/Image.gz-dtb
+SYSTEM_MOD_DIR=${TEMPLATE_SRC_DIR}/modules/system/lib/modules
+VENDOR_MOD_DIR=${TEMPLATE_SRC_DIR}/modules/vendor/lib/modules
+PACKAGE_NAME=${PROJECT_NAME}-${DEVICE_NAME}-${BUILD_TIMESTAMP}-${BUILD_REVISION}.zip
+PACKAGE_ZIP=${OUT_DIR}/${PACKAGE_NAME}
 
 
 # # # SET GLOBAL VARIABLES # # #
 
 export ARCH=arm64
-if [ "${BUILD_HOST_ARCH}" == "x86_64" ]; then
-  export CROSS_COMPILE=${CROSS_COMPILE_PATH}/bin/${CROSS_COMPILE_SUFFIX}
+if [ "${HOST_ARCH}" == "x86_64" ]; then
+  export CROSS_COMPILE=${GCC_DIR}/bin/${GCC_BINARY}
 fi
-export LOCALVERSION=~${PRODUCT_NAME}-${BUILD_REVISION}
+export LOCALVERSION=~${PROJECT_NAME}-${BUILD_REVISION}
 
 
 # # # FUNCTIONS # # #
 
-function verify_toolchain() {
-  if [ "${BUILD_HOST_ARCH}" == "x86_64" ] && [ "${USE_CROSS_COMPILE_REPO}" == true ]; then
-    if [ ! -d "${CROSS_COMPILE_PATH}" ]; then
-      git clone ${CROSS_COMPILE_REPO} ${CROSS_COMPILE_PATH} \
-          -b ${CROSS_COMPILE_BRANCH}
+function verify_gcc() {
+  if [ "${HOST_ARCH}" == "x86_64" ] && [ "${USE_GCC_REMOTE}" == true ]; then
+    if [ ! -d "${GCC_DIR}" ]; then
+      git clone ${GCC_REMOTE} ${GCC_DIR} \
+          -b ${GCC_BRANCH}
     else
-      cd ${CROSS_COMPILE_PATH}
+      cd ${GCC_DIR}
       git fetch
-      git checkout ${CROSS_COMPILE_BRANCH}
+      git checkout ${GCC_BRANCH}
       git pull
-      cd ${BUILD_DIR}
+      cd ${KERNEL_SRC_DIR}
     fi
     echo ""
   fi
 }
 
 function verify_template() {
-  if [ ! -d "${BUILD_DIR_ZIP_TEMPLATE}" ]; then
-    git clone ${ZIP_TEMPLATE_REPO} ${BUILD_DIR_ZIP_TEMPLATE} \
-        -b ${ZIP_TEMPLATE_BRANCH}
+  if [ ! -d "${TEMPLATE_SRC_DIR}" ]; then
+    git clone ${TEMPLATE_REMOTE} ${TEMPLATE_SRC_DIR} \
+        -b ${TEMPLATE_BRANCH}
   else
-    cd ${BUILD_DIR_ZIP_TEMPLATE}
+    cd ${TEMPLATE_SRC_DIR}
     git fetch
-    git checkout ${ZIP_TEMPLATE_BRANCH}
+    git checkout ${TEMPLATE_BRANCH}
     git reset --hard @{u}
-    cd ${BUILD_DIR}
+    cd ${KERNEL_SRC_DIR}
   fi
   echo ""
 }
 
-function clean() {
-  rm -rf ${BUILD_DIR_OUT_OBJ}
+function clean_output() {
+  rm -rf ${KERNEL_OUT_DIR}
   rm -f ${KERNEL_IMG}
-  rm -f ${KERNEL_MOD_SYSTEM}/*.ko
-  rm -f ${KERNEL_MOD_VENDOR}/*.ko
-  rm -f ${BUILD_DIR_ZIP_TEMPLATE}/version
-  rm -f ${BUILD_DIR_OUT}/*.zip
+  rm -f ${SYSTEM_MOD_DIR}/*.ko
+  rm -f ${VENDOR_MOD_DIR}/*.ko
+  rm -f ${TEMPLATE_SRC_DIR}/version
+  rm -f ${OUT_DIR}/*.zip
 }
 
-function build() {
-  mkdir ${BUILD_DIR_OUT_OBJ}
+function build_kernel() {
+  mkdir ${KERNEL_OUT_DIR}
 
-  make O=${BUILD_DIR_OUT_OBJ} ${KERNEL_DEFCONFIG}
+  make O=${KERNEL_OUT_DIR} ${KERNEL_DEFCONFIG}
   echo ""
 
   if [ "$USE_CCACHE" == true ]; then
-    make O=${BUILD_DIR_OUT_OBJ} -j${BUILD_JOB_NUMBER} \
+    make O=${KERNEL_OUT_DIR} -j${HOST_PROC_NUMBER} \
         CC="ccache ${CROSS_COMPILE}gcc" CPP="ccache ${CROSS_COMPILE}gcc -E" || exit 1
   else
-    make O=${BUILD_DIR_OUT_OBJ} -j${BUILD_JOB_NUMBER} || exit 1
+    make O=${KERNEL_OUT_DIR} -j${HOST_PROC_NUMBER} || exit 1
   fi
   echo ""
 }
 
 function strip_modules() {
-  find ${BUILD_DIR_OUT_OBJ} \
+  find ${KERNEL_OUT_DIR} \
       -name "*.ko" \
       -exec ${CROSS_COMPILE}strip --strip-debug --strip-unneeded {} \;
 }
 
 function sign_modules() {
-  if [ -f "${BUILD_DIR_OUT_OBJ}/certs/signing_key.pem" ]; then
-    find ${BUILD_DIR_OUT_OBJ} \
+  if [ -f "${KERNEL_OUT_DIR}/certs/signing_key.pem" ]; then
+    find ${KERNEL_OUT_DIR} \
         -name "*.ko" \
-        -exec ${BUILD_DIR_OUT_OBJ}/scripts/sign-file sha512 \
-              ${BUILD_DIR_OUT_OBJ}/certs/signing_key.pem \
-              ${BUILD_DIR_OUT_OBJ}/certs/signing_key.x509 {} \;
+        -exec ${KERNEL_OUT_DIR}/scripts/sign-file sha512 \
+              ${KERNEL_OUT_DIR}/certs/signing_key.pem \
+              ${KERNEL_OUT_DIR}/certs/signing_key.x509 {} \;
   fi
 }
 
-function copy_kernel() {
-  cp -v ${BUILD_DIR_OUT_OBJ}/arch/arm64/boot/Image.gz-dtb ${KERNEL_IMG}
+function copy_image() {
+  cp -v ${KERNEL_OUT_DIR}/arch/arm64/boot/Image.gz-dtb ${KERNEL_IMG}
 
-  echo "Version: ${LINUX_VERSION}-perf~${PRODUCT_NAME}-${BUILD_REVISION}" > ${BUILD_DIR_ZIP_TEMPLATE}/version
+  echo "Version: ${LINUX_VERSION}-perf~${PROJECT_NAME}-${BUILD_REVISION}" > ${TEMPLATE_SRC_DIR}/version
 
   echo ""
 }
 
 function copy_modules() {
-  find ${BUILD_DIR_OUT_OBJ} \
+  find ${KERNEL_OUT_DIR} \
       -name "*.ko" \
-      -exec cp -v {} ${KERNEL_MOD_SYSTEM} \;
-  if [ ! -d "${KERNEL_MOD_VENDOR}" ]; then
-    mkdir -p ${KERNEL_MOD_VENDOR}
+      -exec cp -v {} ${SYSTEM_MOD_DIR} \;
+  if [ ! -d "${VENDOR_MOD_DIR}" ]; then
+    mkdir -p ${VENDOR_MOD_DIR}
   fi
-  if [ -f "${KERNEL_MOD_SYSTEM}/wlan.ko" ]; then
-    mv -v ${KERNEL_MOD_SYSTEM}/wlan.ko ${KERNEL_MOD_SYSTEM}/qca_cld3_wlan.ko
-    cp -v ${KERNEL_MOD_SYSTEM}/qca_cld3_wlan.ko ${KERNEL_MOD_VENDOR}/qca_cld3_wlan.ko
+  if [ -f "${SYSTEM_MOD_DIR}/wlan.ko" ]; then
+    mv -v ${SYSTEM_MOD_DIR}/wlan.ko ${SYSTEM_MOD_DIR}/qca_cld3_wlan.ko
+    cp -v ${SYSTEM_MOD_DIR}/qca_cld3_wlan.ko ${VENDOR_MOD_DIR}/qca_cld3_wlan.ko
   fi
-  if [ -f "${KERNEL_MOD_SYSTEM}/msm_11ad_proxy.ko" ] && [ -f "${KERNEL_MOD_SYSTEM}/wil6210.ko" ]; then
-    cp -v ${KERNEL_MOD_SYSTEM}/msm_11ad_proxy.ko ${KERNEL_MOD_VENDOR}/msm_11ad_proxy.ko
-    cp -v ${KERNEL_MOD_SYSTEM}/wil6210.ko ${KERNEL_MOD_VENDOR}/wil6210.ko
+  if [ -f "${SYSTEM_MOD_DIR}/msm_11ad_proxy.ko" ] && [ -f "${SYSTEM_MOD_DIR}/wil6210.ko" ]; then
+    cp -v ${SYSTEM_MOD_DIR}/msm_11ad_proxy.ko ${VENDOR_MOD_DIR}/msm_11ad_proxy.ko
+    cp -v ${SYSTEM_MOD_DIR}/wil6210.ko ${VENDOR_MOD_DIR}/wil6210.ko
   fi
   echo ""
 }
 
 function build_zip() {
-  cd ${BUILD_DIR_ZIP_TEMPLATE}
-  zip -r9 ${PACKAGE_PATH} * \
+  cd ${TEMPLATE_SRC_DIR}
+  zip -r9 ${PACKAGE_ZIP} * \
       -x .git README.md *placeholder patch/ ramdisk/
-  cd ${BUILD_DIR}
+  cd ${KERNEL_SRC_DIR}
 
   echo ""
-  echo "out: ${PACKAGE_PATH}"
+  echo "out: ${PACKAGE_ZIP}"
   echo ""
 }
 
 
 # # # MAIN FUNCTION # # #
 
-if [ ! -d "${BUILD_DIR_OUT}" ]; then
-  mkdir ${BUILD_DIR_OUT}
+if [ ! -d "${OUT_DIR}" ]; then
+  mkdir ${OUT_DIR}
 fi
 
 (
-  verify_toolchain
+  verify_gcc
   verify_template
-  clean
-  build
-  copy_kernel
+  clean_output
+  build_kernel
+  copy_image
   strip_modules
   sign_modules
   copy_modules
   build_zip
-) 2>&1 | tee ${BUILD_DIR_OUT}/build.log
+) 2>&1 | tee ${OUT_DIR}/build.log
